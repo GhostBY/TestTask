67bccb4181cf29b5eb64195e44df25cb2746f2fe
Lukashov Alexandr <alexandrlukashov1997@gmail.com>
2017-10-06 15:52:49 +0300

    add  Company and Employee entities

---
+++ DAL/Entities/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Company
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string OrganizationalForm { get; set; }
+    }
+}
---
+++ DAL/Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+   public class Employee
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Middlename { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public string Position { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
