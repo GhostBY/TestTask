10dcdf267766be7259c59d8927776d6ed97a9dc3
Lukashov Alexandr <alexandrlukashov1997@gmail.com>
2017-10-06 18:13:14 +0300

    add System.Configuration reference and DBConnection class

--- DAL/DBConnection.cs
+++ DAL/DBConnection.cs
@@ -4,15 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace DAL
 {
-    public static class DBConnection
+    public  class DBConnection
     {
-        private static SqlConnection Connection = null;
-         public void OpenConnection()
-        {
+        private  SqlConnection Connection = null;
+        private  SqlDataAdapter Adapter = null;
 
+        public DBConnection()
+        {
+            Adapter = new SqlDataAdapter();
+            Connection = new SqlConnection(ConfigurationManager.ConnectionStrings
+                    ["DefaultConnection"].ConnectionString);
+        }
+        private SqlConnection OpenConnection()
+        {
+            if(Connection.State==ConnectionState.Closed||Connection.State==ConnectionState.Broken)
+            {
+                Connection.Open();
+            }
+            return Connection;
+        }
+        public DataTable ExecuteSelectQuery(string Query, SqlParameter[] Parameters)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            DataTable dataTable = new DataTable();
+            dataTable = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                myCommand.Connection = OpenConnection();
+                myCommand.CommandText = Query;
+                myCommand.Parameters.AddRange(Parameters);
+                myCommand.ExecuteNonQuery();
+                Adapter.SelectCommand = myCommand;
+                Adapter.Fill(ds);
+                dataTable = ds.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                return null;
+            }
+            finally
+            {
+            }
+            return dataTable;
+        }
+
+        public bool ExecuteInsertQuery(string Query, SqlParameter [] Parameters)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = OpenConnection();
+                myCommand.CommandText = Query;
+                myCommand.Parameters.AddRange(Parameters);
+                Adapter.InsertCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                return false;
+            }
+            finally
+            {
+            }
+            return true;
+        }
+
+        public bool ExecuteUpdateQuery(string Query, SqlParameter[] Parameters)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = OpenConnection();
+                myCommand.CommandText = Query;
+                myCommand.Parameters.AddRange(Parameters);
+                Adapter.UpdateCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                return false;
+            }
+            finally
+            {
+            }
+            return true;
         }
     }
 }
